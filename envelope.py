d68d92742083ca1e71684b7028c39a8b7a377ac1
Yusuke Murakami <carrot.yummy@gmail.com>
2014-05-30 01:20:42 +0900

    ver2.0.0　あとは誤動作を防ぐ試みを加えようと思う（nの大きくしすぎを防ぐ、など）

--- envelope.py
+++ envelope.py
@@ -1,22 +1,38 @@
+from mpl_toolkits.axes_grid.axislines import SubplotZero
 import matplotlib.pyplot as plt
 import numpy as np
+import pylab as pl
 
-def subplots():
-    "Custom subplots with axes throught the origin"
-    fig, ax = plt.subplots()
+n = 200 # define the number of lines
 
-    # Set the axes through the origin
-    for spine in ['left', 'bottom']:
-        ax.spines[spine].set_position('zero')
-    for spine in ['right', 'top']:
-        ax.spines[spine].set_color('none')
+def f(x,a): # define the function, x as parameter 
+    return -x**2+a*x
+
+def subplots(): # learn by "http://matplotlib.org/examples/axes_grid/demo_axisline_style.html"
     
-    ax.grid()
+    fig = plt.figure(1)
+    ax = SubplotZero(fig, 111)
+    fig.add_subplot(ax)
+
+    for direction in ["xzero", "yzero"]:# define the shape of axes
+        ax.axis[direction].set_axisline_style("-|>")
+        ax.axis[direction].set_visible(True)
+
+    for direction in ["left", "right", "bottom", "top"]:# hide the ticks around the graph
+        ax.axis[direction].set_visible(False)
+
     return fig, ax
 
-fig, ax = subplots()  # Call the local version, not plt.subplots()
-x = np.linspace(-2, 10, 200)
-y = x
-ax.plot(x, y, 'r-', linewidth=2, label='sine function', alpha=0.6)
-ax.legend(loc='lower right')
+fig, ax = subplots()  # call the local version, not plt.subplots()
+
+plt.axis([-n**0.8, n**0.8, -n*0.02, n]) # the range of graph (xmin,xmax,ymin,ymax)
+
+plt.xticks([0]) # plot the origin point on the x-axis
+plt.yticks([]) # plot nothing on the y-axis
+
+a = np.linspace(-2*n, 2*n, 100*n) # the range of each line (xmin,xmax,***)
+
+for x in range(n):
+    y = f(x-n/2+0.5, a)
+    ax.plot(a, y, 'r-', linewidth=2) # draw lines
 plt.show()
